79945f5b499c23f133f905b0740b5c5e4ea5d174
Abhishek Das <abhi.das2007das@gmail.com>
2020-02-02 19:34:25 +0000

    fix(describeKeyPair): lint issues

--- src/services/KeyPair/describeKeyPair.js
+++ src/services/KeyPair/describeKeyPair.js
@@ -1,5 +1,5 @@
 import { EC2 } from 'aws-sdk';
-import { REGION, API_VERSIONS} from '../../constants';
+import { REGION, API_VERSIONS } from '../../constants';
 
 const ec2 = new EC2({ region: REGION, apiVersion: API_VERSIONS.ec2 });
 
